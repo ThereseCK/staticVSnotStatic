a72985ffece87b6ce84b9d0b16a3c6efb9c68b1e
therese camilla nordnes <59839349+ThereseCK@users.noreply.github.com>
2020-05-19 09:14:20 +0200

    Initial commit

---
+++ staticVSnotStatic/Place.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace objekter_via_static
+{
+    class Place
+    {
+        public string PlaceName { get; private set; }
+        public string Municipality { get; private set; }
+        public string Region { get; private set; }
+
+        public Place(string placeName, string municipality, string region)
+        {
+            PlaceName = placeName;
+            Municipality = municipality;
+            Region = region;
+        }
+
+
+        public void ShowPlace()
+        {
+            var labelWidth = 8;
+            ShowSeperatorRow(labelWidth);
+            ShowFieldNameAndValue("Navn", labelWidth, PlaceName);
+            ShowFieldNameAndValue("Kommune", labelWidth, Municipality);
+            ShowFieldNameAndValue("Fylke", labelWidth, Region);
+
+        }
+        private void ShowFieldNameAndValue(string label, int labelWidth, string fieldValue)
+        {
+            labelWidth -= label.Length;
+            Console.WriteLine("    " + label + ":" + " ".PadLeft(labelWidth, ' ') + fieldValue);
+        }
+
+
+        private void ShowSeperatorRow(int labelWidth)
+        {
+            labelWidth += 15;
+            Console.WriteLine(string.Empty.PadLeft(labelWidth, '-'));
+        }
+    }
+}
---
+++ staticVSnotStatic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace objekter_via_static
+{
+    class Program //C# 
+    {
+        static void Main(string[] args)
+        {
+            var place = new Place("Verningen", "Larvik", "Vestfold");
+            var place2 = new Place("Haukerød", "Sandefjord", "Vestfold");
+            var place3 = new Place("Øygarden", "Bergen", "Hordaland");
+
+            place.ShowPlace();
+            place2.ShowPlace();
+            place3.ShowPlace();
+            /*Console.WriteLine("******************************");
+            Console.WriteLine("       Navn:       Stavern");
+            Console.WriteLine("       Kommune:    Larvik");
+            Console.WriteLine("       Fylke:      Vestfold");
+            Console.WriteLine("******************************");*/
+        }
+
+    }
+}
